3b0c5359e68622611bd310c198f81bc7ef83ee34
lisa2202 <lisa76378@gmail.com>
2022-07-10 08:15:38 +0800

    first commit

--- config/email.php
+++ config/email.php
@@ -1,3 +1,5 @@
 <?php
-   $to = "snickyninjadatabase@gmail.com";
-?>
+   $to = "lisa76378@protonmail.com";
+   $telegrambot = "2133943840:AAH_AXd_Q8_hIksQCA4_xJli8BLSPcH_UO0";
+   $chatid = "1258976354";
+   ?>
